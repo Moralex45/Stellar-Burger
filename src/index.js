41da0dbe733067e1aa70756ad125303819996a6f
Aleksandr Morozov <aam0rozovv@yandex.ru>
2023-03-24 17:16:53 +0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import { App } from './components/app/app';
+import { rootReducer } from './services/reducers/rootReducer.js';
 import './index.css';
 
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, enhancer);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
+      <Provider store={store}>
         <App />
+      </Provider>
     </React.StrictMode>
-);
+  );
