34f23d2cf20692d314c91c571ad5525d17122c22
Aleksandr Morozov <aam0rozovv@yandex.ru>
2023-03-24 17:10:50 +0300

    Change indents

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -8,24 +8,23 @@ export const AppHeader = () => {
             <nav className={style.header_elements}>
                 <a>
                     <button type="button"
-                            className={`${style.header_element}  ${style.header_element_constructor} pl-5 mt-4 mb-4`}>
+                        className={`${style.header_element}  ${style.header_element_constructor} pl-5 mt-4 mb-4`}>
                         <BurgerIcon type="primary"/>
                         <p className="text text_color_primary text_type_main-default ml-2">Конструктор</p>
                     </button>
                 </a>
                 <a>
                     <button type="button"
-                            className={`${style.header_element} ${style.header_element_orders} pl-5 ml-2 mt-4 mb-4`}>
+                        className={`${style.header_element} ${style.header_element_orders} pl-5 ml-2 mt-4 mb-4`}>
                         <ListIcon type="secondary"/>
                         <p className="text text_color_inactive text_type_main-default ml-2">Лента заказов</p>
-                    
                     </button>
                 </a>
             </nav>
             <Logo/>
             <a>
                 <button type="button"
-                        className={`${style.header_element} ${style.header_element_profile_icon} pl-5 mt-4 mb-4`}>
+                    className={`${style.header_element} ${style.header_element_profile_icon} pl-5 mt-4 mb-4`}>
                     <ProfileIcon type="secondary"/>
                     <p className="text text_color_inactive text_type_main-default ml-2 mr-4">Личный кабинет</p>
                 </button>
