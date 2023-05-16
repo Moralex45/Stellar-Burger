8cec9cf5d169d4920879b01c91d9606f05e70e82
Aleksandr Morozov <aam0rozovv@yandex.ru>
2023-05-16 12:40:14 +0300

    Creating an order page. Creating styles

---
+++ src/pages/ordersPage/ordersPage.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, FC } from 'react';
+import { useSelector, useDispatch } from '../../services/types/hooks';
+import { NavLink, useLocation } from 'react-router-dom';
+
+import style from './ordersPage.module.css';
+
+import { OrderItem } from '../../components/orderItem/orderItem';
+import { Loader } from '../../components/loader/loader';
+import { wsUserOrdersConnectionStart, wsUserOrdersConnectionDisconnect } from '../../services/actions/ordersActions';
+
+export const OrdersPage: FC = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const userOrders = useSelector((state) => state.ordersReducer.userOrders);
+  const wsUserOrdersConnectFailed = useSelector((state) => state.ordersReducer.wsUserOrdersConnectFailed);
+  
+  useEffect(() => {
+    dispatch(wsUserOrdersConnectionStart());
+    if (wsUserOrdersConnectFailed) {
+      dispatch(wsUserOrdersConnectionStart());
+    }
+    return () => {
+      dispatch(wsUserOrdersConnectionDisconnect())
+    }
+  }, [dispatch, wsUserOrdersConnectFailed]);
+
+  return (
+    <section className={style.container}>
+
+      <div className={style.scrollbar}>
+        <ul className={style.list}>
+      
+          {userOrders.length > 0 ? (
+            userOrders?.map((order, index) => (
+              <NavLink className={style.link} to={`/profile/orders/${order._id}`} key={index} state={{ previousLocationOrders: location }}>
+                <OrderItem order={order} isUserOrders={true} />
+              </NavLink>
+            ))
+          ) : (
+            <Loader />
+          )}
+
+        </ul>
+      </div>
+    </section>
+  )
+};
+
+export default OrdersPage;
